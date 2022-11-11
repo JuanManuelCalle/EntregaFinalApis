1c0e6643f34a017395ac4d9b1e9ffbf950d3455e
JuanManuelCalle <114536103+JuanManuelCalle@users.noreply.github.com>
2022-11-11 18:40:11 -0500

    Se suben Apis para ejecutar en Visual Studio

---
+++ ConsignaFinal/ConsignaFinal/Controllers/InicioSesionController.cs
@@ -0,0 +1,19 @@
+﻿using ConsignaFinal.Models;
+using ConsignaFinal.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsignaFinal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InicioSesionController : ControllerBase
+    {
+        [HttpGet("{usuario}/{contrasena}")]
+        public List<Usuario> Login(string usuario, string contrasena)
+        {
+            var usuarioFinal = Ado_UsuarioModificacion.Login(usuario,contrasena);
+            return usuarioFinal == null ? new List<Usuario>() : usuarioFinal;
+        }
+    }
+}
